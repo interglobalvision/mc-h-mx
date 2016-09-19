efcd9f81725d93e1de56030982b25d8412f436d3
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-19 12:51:53 -0500

    add projects to home with layout options

--- index.php
+++ index.php
@@ -1,32 +1,41 @@
 <?php
 get_header();
+
+$projects = IGV_get_option('_igv_home_layout_options', '_igv_home_projects');
 ?>
 
 <main id="main-content">
   <section id="posts">
-
+    <div class="container">
+      <div class="grid-row">
 <?php
-if( have_posts() ) {
-  while( have_posts() ) {
-    the_post();
+if ($projects) {
+  foreach ($projects as $project) {
+    if ($project['id']) {
+      $single_row = $project['single_row'];
+      $top_margin = !empty($project['top_margin']) ? $project['top_margin'] . 'px' : '0';
+      $side_margin = !empty($project['side_margin']) ? $project['side_margin'] . '%' : '0';
+      $percent_width = !empty($project['percent_width']) ? $project['percent_width'] . '%' : '0';
 ?>
 
-    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+    <article id="post-<?php echo $project['id']; ?>" style="margin-top: <?php _e($top_margin); ?>; left: <?php _e($side_margin); ?>;" class="text-align-center grid-item item-s-12<?php echo $single_row != 'on' ? ' item-m-6"' : '"';?>>
 
-      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-
-      <?php the_content(); ?>
+      <a href="<?php echo get_the_permalink($project['id']) ?>">
+        <?php echo wp_get_attachment_image( $project['image_id'], 'full', false, array('style' => 'max-width: ' . $percent_width) ); ?>
+      </a>
 
     </article>
 
 <?php
+    }
   }
 } else {
 ?>
     <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
-
+      </div>
+    </div>
   </section>
 
   <?php get_template_part('partials/pagination'); ?>
