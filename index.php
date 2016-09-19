9cdbed38edf5bff4b442a7e3552ac2e1a9003fc3
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-19 14:44:38 -0500

    move home into front-page. restore index. remove home-layout-options page

--- index.php
+++ index.php
@@ -1,45 +1,35 @@
 <?php
 get_header();
-
-$projects = IGV_get_option('_igv_home_layout_options', '_igv_home_projects');
 ?>
 
 <main id="main-content">
-  <div class="cam-feed"></div>
   <section id="posts">
-    <div class="container">
-      <div class="grid-row">
+
 <?php
-if ($projects) {
-  foreach ($projects as $project) {
-    if ($project['id']) {
-      $single_row = $project['single_row'];
-      $top_margin = !empty($project['top_margin']) ? $project['top_margin'] . 'px' : '0';
-      $side_margin = !empty($project['side_margin']) ? $project['side_margin'] . '%' : '0';
-      $percent_width = !empty($project['percent_width']) ? $project['percent_width'] . '%' : '0';
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
 ?>
 
-    <article id="post-<?php echo $project['id']; ?>" style="margin-top: <?php _e($top_margin); ?>; left: <?php _e($side_margin); ?>;" class="home-project-item text-align-center grid-item item-s-12<?php echo $single_row != 'on' ? ' item-m-6"' : '"';?>>
+    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-      <a href="<?php echo get_the_permalink($project['id']) ?>">
-        <?php echo wp_get_attachment_image( $project['image_id'], 'full', false, array('style' => 'max-width: ' . $percent_width) ); ?>
-      </a>
+      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+
+      <?php the_content(); ?>
 
     </article>
 
 <?php
-    }
   }
 } else {
 ?>
     <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
-      </div>
-    </div>
+
   </section>
 
-  <?php get_template_part('partials/cam-control'); ?>
+  <?php get_template_part('partials/pagination'); ?>
 
 </main>
 
--- lib/theme-options/theme-options.php
+++ lib/theme-options/theme-options.php
@@ -5,4 +5,3 @@ get_template_part( 'lib/theme-options/theme-options-class' );
 
 // One file per options page
 get_template_part( 'lib/theme-options/site-options' );
-get_template_part( 'lib/theme-options/home-layout-options' );
