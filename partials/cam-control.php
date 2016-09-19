88f06a073d985f1bb1b305fddd466bad14a20919
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-19 13:59:00 -0500

    update cam ui attrs

--- partials/cam-control.php
+++ partials/cam-control.php
@@ -2,19 +2,19 @@
   <div class="container">
     <div class="grid-row">
       <div class="grid-item item-s-8 item-m-7 item-l-5 grid-column justify-center align-items-start">
-        <input id="cam-slider" class="cam-ui cam-slider" type="range">
+        <input class="cam-ui cam-fader" type="range">
       </div>
       <div class="grid-item item-s-4 item-m-5 item-l-7 grid-row justify-end">
         <div class="cam-buttons grid-column justify-between">
           <div class="grid-item no-gutter text-align-center">
-            <button id="cam-up" class="cam-ui"></button>
+            <button data-command="up" class="cam-ui cam-button"></button>
           </div>
           <div class="grid-item no-gutter grid-row justify-between">
-            <button id="cam-right" class="cam-ui"></button>
-            <button id="cam-left" class="cam-ui"></button>
+            <button data-command="right" class="cam-ui cam-button"></button>
+            <button data-command="left" class="cam-ui cam-button"></button>
           </div>
           <div class="grid-item no-gutter text-align-center">
-            <button id="cam-down" class="cam-ui"></button>
+            <button data-command="down" class="cam-ui cam-button"></button>
           </div>
         </button>
       </div>
