5ded60205358fccd147ff67cf40d58ec810f1d45
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-09-19 20:59:53 -0500

    set project list margin and font-size

--- partials/project-list.php
+++ partials/project-list.php
@@ -1,4 +1,4 @@
-<div class="grid-item item-s-12 item-m-10 item-l-8 project-list">
+<div class="grid-item item-s-12 item-m-10 item-l-8 project-list margin-top-small">
   <ul>
 <?php 
 $projects = get_posts('post_type=project');
@@ -6,7 +6,7 @@ $projects = get_posts('post_type=project');
 if ($projects) {
   foreach ($projects as $project) {
 ?>
-    <li><a href="<?php echo get_the_permalink($project->ID); ?>"><?php echo get_the_title($project->ID); ?></a></li>
+    <li class="font-size-h1"><a href="<?php echo get_the_permalink($project->ID); ?>"><?php echo get_the_title($project->ID); ?></a></li>
 <?php
   }
 }
