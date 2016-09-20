026d0dbc6344023797637a935174993c11502745
cas <cas@interglobal.vision>
2016-09-20 15:01:54 -0500

    add projects rewrite

--- lib/post-types.php
+++ lib/post-types.php
@@ -52,7 +52,9 @@ function register_cpt_project() {
     'has_archive' => true,
     'query_var' => true,
     'can_export' => true,
-    'rewrite' => true,
+    'rewrite' => array(
+      'slug' => 'projects',
+    ),
     'capability_type' => 'post'
   );
 
