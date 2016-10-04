75e1ea2dd01af1d4c411ae1d06ef23d0d2a68208
Patrick Best <patrickbest@patrickbest.com>
2016-10-04 18:18:19 -0500

    update single project

--- partials/project-list.php
+++ partials/project-list.php
@@ -1,12 +1,13 @@
-<div class="grid-item item-s-12 item-m-10 item-l-8 project-list margin-top-small">
+<div class="grid-item item-s-12 item-m-10 item-l-8 project-list">
   <ul>
-<?php 
+<?php
 $projects = get_posts('post_type=project');
+$current_project_id = $post->ID;
 
 if ($projects) {
   foreach ($projects as $project) {
 ?>
-    <li class="font-size-h1"><a href="<?php echo get_the_permalink($project->ID); ?>"><?php echo get_the_title($project->ID); ?></a></li>
+    <li <?php if ($project->ID === $current_project_id) {echo 'class="font-italic"';} ?>><a href="<?php echo get_the_permalink($project->ID); ?>"><?php echo get_the_title($project->ID); ?></a></li>
 <?php
   }
 }
