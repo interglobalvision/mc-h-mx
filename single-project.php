3dd0504e6ac8052fb5ff76ce2eb3707f02911a86
Michael Ray-Von <michaelray-von@Michaels-MacBook-Pro.local>
2016-10-04 15:19:22 -0500

    remove extra header. move container elements outside loop

--- single-project.php
+++ single-project.php
@@ -1,16 +1,14 @@
 <?php
 get_header();
 ?>
-
+<main id="main-content">
+  <section id="posts">
+    <div class="container">
 <?php
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
 ?>
-
-<main id="main-content">
-  <section id="posts">
-    <div class="container">
       <article <?php post_class('grid-row'); ?> id="post-<?php the_ID(); ?>">
 
         <div class="grid-item item-s-12 item-l-9 project-title margin-bottom-small">
@@ -26,38 +24,16 @@ if( have_posts() ) {
       <div class="grid-row">
         <?php get_template_part('partials/project-list'); ?>
       </div>
-    </div>
-  </section>
-
-</main>
-
 <?php
   }
 } else {
 ?>
-<header id="header">
-  <div class="container">
-    <div class="grid-row">
-      <div class="grid-item item-l-9">
-        <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-      </div>
-      <div class="grid-item item-l-3">
-
-      </div>
-    </div>
-  </div>
-</header>
-
-<main id="main-content">
-  <section id="posts">
-    <div class="container">
       <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+<?php
+} ?>
     </div>
   </section>
 </main>
-<?php
-} ?>
-
 <?php
 get_footer();
 ?>
