1ea02eac0141c0ee4751b8ed3fcf33d78b50eb8c
cas <cas@interglobal.vision>
2016-10-05 14:06:42 -0500

    add button to view the website from the studio to the admin bar

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -1,3 +1,12 @@
 <?php
 
 // Custom functions (like special queries, etc)
+function custom_view_from_studio_button($wp_admin_bar){
+  $args = array(
+    'id' => 'from-studio-button',
+    'title' => 'View website from studio',
+    'href' => get_site_url() . '?isLocal=true',
+  );
+  $wp_admin_bar->add_node($args);
+}
+add_action('admin_bar_menu', 'custom_view_from_studio_button', 50);
