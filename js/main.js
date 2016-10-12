c9bc505a1dbbecb532941c24970318c1c2d49069
Patrick Best <patrickbest@patrickbest.com>
2016-10-11 19:13:20 -0500

    impliment cookie and tidy js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* jshint browser: true, devel: true, indent: 2, curly: true, eqeqeq: true, futurehostile: true, latedef: true, undef: true, unused: true */
-/* global $, jQuery, document, Site, Modernizr */
+/* global $, jQuery, document, Site, Cookies */
 
 Site = {
   mobileThreshold: 601,
@@ -10,10 +10,11 @@ Site = {
       _this.onResize();
     });
 
+    if ($('.cam-feed').length) {
+      _this.Camera.init();
+    }
+
     $(document).ready(function () {
-      if ($('.cam-feed').length) {
-        _this.Camera.init();
-      }
 
       if ($('.lang-toggle').length) {
         _this.bindLangToggle();
@@ -45,36 +46,60 @@ Site = {
 
 Site.Camera = {
   init: function() {
-    _this = this;
+    var _this = this;
+
+    _this.$cameraFeed = $('.cam-feed');
+    _this.$fader = $('.cam-fader');
+    _this.$projectThumbs = $('.home-project-item');
 
     _this.isLocalHost();
     _this.bindButtons();
     _this.bindFader();
+
+    if (Cookies.get('faderValue')) {
+      _this.setFromCookie();
+    }
   },
 
   isLocalHost: function() {
-    _this = this;
+    var _this = this;
 
-    if (location.hostname === "localhost" || _this.getUrlParameter('isLocal')) {
-      _this.isLocal = true; 
+    if (location.hostname === 'localhost' || _this.getUrlParameter('isLocal')) {
+      _this.isLocal = true;
 
       _this.setLocalStyles();
     } else {
-      _this.isLocal = false; 
+      _this.isLocal = false;
     }
 
   },
 
   setLocalStyles: function() {
-    $('.cam-feed').css('background-image', 'url(http://192.168.1.70)');
+    var _this = this;
+
+    _this.$cameraFeed.css('background-image', 'url(http://192.168.1.70)');
+  },
+
+  setFromCookie: function() {
+    var _this = this;
+    var faderValueFromCookie = Cookies.get('faderValue');
+
+    $({ val: 50 }).animate({ val: faderValueFromCookie }, {
+        duration: 100,
+        easing: 'linear',
+        step: function(val) {
+          _this.$fader.val(val);
+          _this.setOpacitiesFromFader(val);
+        }
+    });
   },
 
   bindButtons: function() {
+    var _this = this;
     var command;
-    // CHANGE THIS BASEURL //
     var baseUrl = 'http://estudioherrera.servehttp.com/api/';
 
-    if(_this.isLocal) {
+    if (_this.isLocal) {
       baseUrl = 'http://192.168.1.70/api/';
     }
 
@@ -82,7 +107,7 @@ Site.Camera = {
       command = $(this).attr('data-command');
 
       $.ajax({
-        method: "POST",
+        method: 'POST',
         url: baseUrl + command,
       }).done(function( response ) {
         console.log(response);
@@ -91,20 +116,30 @@ Site.Camera = {
   },
 
   bindFader: function() {
-    $(document).on('input', '.cam-fader', function() {
-      var projectOpacity = $(this).val() > 50 ? (100 - $(this).val()) * .02 : 100,
-        camOpacity = $(this).val() < 51 ? $(this).val() * .02 : 100;
+    var _this = this;
 
-      $('.cam-feed').css('opacity', camOpacity);
-      $('.home-project-item').css('opacity', projectOpacity);
+    _this.$fader.on('input', function() {
+      var faderValue = $(this).val();
+
+      Cookies.set('faderValue', faderValue);
+      _this.setOpacitiesFromFader(faderValue);
     });
   },
 
+  setOpacitiesFromFader: function(faderValue) {
+    var _this = this;
+    var projectOpacity = faderValue > 50 ? (100 - faderValue) * .02 : 100;
+    var camOpacity = faderValue < 51 ? faderValue * .02 : 100;
+
+    _this.$cameraFeed.css('opacity', camOpacity);
+    _this.$projectThumbs.css('opacity', projectOpacity);
+  },
+
   getUrlParameter: function(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-      sURLVariables = sPageURL.split('&'),
-      sParameterName,
-      i;
+    var sPageURL = decodeURIComponent(window.location.search.substring(1));
+    var sURLVariables = sPageURL.split('&');
+    var sParameterName;
+    var i;
 
     for (i = 0; i < sURLVariables.length; i++) {
       sParameterName = sURLVariables[i].split('=');
