c01af26a55fd15786f8cc8f59270f2f0cb39ce33
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-10-12 14:28:23 -0500

    insert pagination nav svgs in markup

--- partials/project-pagination.php
+++ partials/project-pagination.php
@@ -1,6 +1,10 @@
-<div class="grid-item item-s-6">
-  <?php previous_post_link('<img class="project-nav-arrow" src="' . get_bloginfo('stylesheet_directory') . '/img/dist/nav-left.svg"> %link '); ?>
+<div class="grid-item item-s-6 project-pagination grid-row justify-start align-items-center">
+  <?php 
+  $left_arrow = url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/nav_left.svg');
+  previous_post_link("$left_arrow %link"); ?>
 </div>
-<div class="grid-item item-s-6 text-align-right">
-  <?php next_post_link('%link <img class="project-nav-arrow" src="' . get_bloginfo('stylesheet_directory') . '/img/dist/nav-right.svg">'); ?>
+<div class="grid-item item-s-6 project-pagination grid-row justify-end align-items-center">
+  <?php 
+  $right_arrow = url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/nav_right.svg');
+  next_post_link("%link $right_arrow"); ?>
 </div>
