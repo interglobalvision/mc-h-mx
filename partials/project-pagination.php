923009e4451c02cc00ffacfb87d463ca5777b92a
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-10-12 16:55:36 -0500

    put arrows inside pagination link

--- partials/project-pagination.php
+++ partials/project-pagination.php
@@ -1,10 +1,10 @@
-<div class="grid-item item-s-6 project-pagination grid-row justify-start align-items-center">
+<div class="grid-item project-pagination">
   <?php 
   $left_arrow = url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/nav_left.svg');
-  previous_post_link("$left_arrow %link"); ?>
+  previous_post_link("%link", "$left_arrow %title"); ?>
 </div>
-<div class="grid-item item-s-6 project-pagination grid-row justify-end align-items-center">
+<div class="grid-item project-pagination">
   <?php 
   $right_arrow = url_get_contents(get_bloginfo('stylesheet_directory') . '/img/dist/nav_right.svg');
-  next_post_link("%link $right_arrow"); ?>
+  next_post_link("%link", "%title $right_arrow"); ?>
 </div>
