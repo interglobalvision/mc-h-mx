1763b9d7a9f04fbd96af9d0b4acbad0e73927394
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-10-21 18:02:34 -0500

    fix fader cap top positions

--- partials/cam-control.php
+++ partials/cam-control.php
@@ -3,7 +3,7 @@ $projects = get_post_meta($post->ID, '_igv_home_projects', true);
 ?>
 <section id="cam-control">
   <div class="container">
-    <div class="grid-row margin-bottom-small">
+    <div class="grid-row margin-bottom-basic">
       <div class="grid-item item-s-12 item-m-10 offset-m-1 item-l-8 offset-l-2 grid-column justify-center align-items-start cam-fader-holder <?php echo empty($projects[0]['id']) ? 'hide-fader' : ''; ?>">
         <input class="cam-ui cam-fader" type="range">
       </div>
