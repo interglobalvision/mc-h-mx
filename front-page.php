f8032c717e80c4e4d3aaafcdf9605f99f9c0fa89
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-12-01 18:40:59 -0600

    add front page gif support

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@ $projects = get_post_meta($post->ID, '_igv_home_projects', true);
 <?php
 if ($projects) {
   foreach ($projects as $project) {
-    if ($project['id']) {
+    if ($project['id'] && $project['image']) {
       $single_row = $project['single_row'];
       $top_margin = !empty($project['top_margin']) ? $project['top_margin'] . 'px' : '0';
       $side_margin = !empty($project['side_margin']) ? $project['side_margin'] . '%' : '0';
@@ -20,9 +20,18 @@ if ($projects) {
 ?>
 
     <article id="post-<?php echo $project['id']; ?>" class="home-project-item text-align-center grid-item item-s-12<?php echo $single_row != 'on' ? ' item-m-6' : false;?>">
-      <div style="margin-top: <?php _e($top_margin); ?>; left: <?php _e($side_margin); ?>;">
+      <div style="margin-top: <?php _e($top_margin); ?>; left: <?php _e($side_margin); ?>;"> 
         <a href="<?php echo get_the_permalink($project['id']) ?>">
-          <?php echo wp_get_attachment_image( $project['image_id'], 'gallery', false, array('style' => 'max-width: ' . $percent_width) ); ?>
+<?php 
+        $check_filetype = wp_check_filetype($project['image']);
+
+        if ($check_filetype['ext'] == 'gif') { 
+          $img_elem = '<img src="' . $project['image'] . '">';
+        } else {
+          $img_elem = wp_get_attachment_image( $project['image_id'], 'gallery', false, array('style' => 'max-width: ' . $percent_width) ); 
+        }
+        echo $img_elem;
+?>
         </a>
       </div>
     </article>
