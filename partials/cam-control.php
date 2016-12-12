02609cb4be140e7d515c208a05c40678b1b32edb
Patrick Best <patrickbest@patrickbest.com>
2016-12-12 13:59:11 -0600

    style faders

--- partials/cam-control.php
+++ partials/cam-control.php
@@ -4,11 +4,15 @@ $projects = get_post_meta($post->ID, '_igv_home_projects', true);
 <section id="cam-control">
   <div class="container">
     <div class="grid-row margin-bottom-basic">
-      <div class="grid-item item-s-12 item-m-10 item-l-6 grid-column justify-center align-items-start cam-fader-holder <?php echo empty($projects[0]['id']) ? 'hide-fader' : ''; ?>">
-        <input id="xfader" class="cam-ui cam-fader" type="range">
+      <div class="grid-item item-s-12 item-m-5 grid-column cam-control-item">
+        <div class="cam-fader-holder <?php echo empty($projects[0]['id']) ? 'hide-fader' : ''; ?> justify-center align-items-start">
+          <input id="xfader" class="cam-ui cam-fader" type="range" value="100">
+        </div>
       </div>
-      <div class="grid-item item-s-12 item-m-10 item-l-6 grid-column justify-center align-items-start cam-fader-holder <?php echo empty($projects[0]['id']) ? 'hide-fader' : ''; ?>">
-        <input id="zoomfader" class="cam-ui cam-fader" type="range" value="0">
+      <div class="grid-item item-s-12 item-m-5 offset-m-1 grid-column cam-control-item">
+        <div class="cam-fader-holder <?php echo empty($projects[0]['id']) ? 'hide-fader' : ''; ?> justify-center align-items-start">
+          <input id="zoomfader" class="cam-ui cam-fader" type="range" value="0">
+        </div>
       </div>
     </div>
   </div>
