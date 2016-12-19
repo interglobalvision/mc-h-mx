ff053e7dea7e65f8a9fc30b0dfbd3382f26e759d
Patrick Best <patrickbest@patrickbest.com>
2016-12-19 15:16:50 -0600

    style faders thinner

--- partials/cam-control.php
+++ partials/cam-control.php
@@ -3,7 +3,7 @@ $projects = get_post_meta($post->ID, '_igv_home_projects', true);
 ?>
 <section id="cam-control">
   <div class="container">
-    <div class="grid-row margin-bottom-basic">
+    <div class="grid-row margin-bottom-small">
       <div class="grid-item item-s-12 item-m-5 grid-column cam-control-item">
         <div class="cam-fader-holder <?php echo empty($projects[0]['id']) ? 'hide-fader' : ''; ?> justify-center align-items-start">
           <input id="xfader" class="cam-ui cam-fader" type="range" value="50">
